f04fb58d9ba527204407290ff943f76ea8a15543
sham9394 <165818287+sham9394@users.noreply.github.com>
2025-08-07 11:30:40 +0530

    Update TestApplication.java

--- src/main/java/com/priacc/test/TestApplication.java
+++ src/main/java/com/priacc/test/TestApplication.java
@@ -1,13 +1,12 @@
 package com.priacc.test;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class TestApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
-	}
-
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String hello() {
+        return "✅ App is Running!";
+    }
 }
